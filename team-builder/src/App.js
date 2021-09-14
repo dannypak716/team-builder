6800bc9019eb5964fc9b270dab954e2b1a07fb50
Jeong Pak <dannypak716@gmail.com>
2021-09-14 16:05:53 -0400

    user can render additional members and their info thru input box

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -3,49 +3,71 @@ import './App.css';
 import React, { useState } from 'react';
 
 const teamList = [
-  { name: 'Danny', age: '24', favFood: 'tacos' },
-  { name: 'Jared', age: '29', favFood: 'dirt' },
-  { name: 'Lance', age: '22', favFood: 'pasta' },
-  { name: 'Ryan', age: '25', favFood: 'water' },
-  { name: 'Shane', age: '23', favFood: 'cheese' },
+  { name: 'Danny', email: 'danny@gmail.com', role: 'Team Lead' },
+  { name: 'Jared', email: 'jared@gmail.com', role: 'Front End Engineer' },
+  { name: 'Lance', email: 'lance@gmail.com', role: 'Full Stack Engineer' },
+  { name: 'Ryan', email: 'ryan@gmail.com', role: 'Back End Engineer' },
+  { name: 'Shane', email: 'shane@gmail.com', role: 'Designer' },
 ]
 
-const initialFormValues = { name: '', age: '', favFood: '' }
+const initialFormValues = { name: '', email: '', role: '' }
 
 function App() {
   const [members, setMembers] = useState(teamList);
   const [formValues, setFormValues] = useState(initialFormValues);
+
+  const change = (evt) => {
+    const {name, value} = evt.target;
+    setFormValues({...formValues, [name]: value })
+  }
+
+  const submit = (evt) => {
+    evt.preventDefault();
+
+    const newMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role.trim()
+    }
+
+    setMembers(members.concat(newMember));
+    setFormValues(initialFormValues);
+  }
 
   return (
     <div className="App">
       <h1>List of Team Members</h1>
       {members.map((member, idx) => (
         <div key={idx}>
-          {member.name} is {member.age} years old and loves to eat {member.favFood}
-        </div>
+          {member.name}: {member.role} <br/> {member.email}
+        <br/></div>
       ))}
-      <form>
+      <form onSubmit={submit}>
         <input 
           type="text"
           name="name"
           placeholder="Wusyaname?"
           value={formValues.name}
+          onChange={change}
         >
         </input>
         <input 
           type="text"
-          name="age"
-          placeholder="# of Sun revolutions"
-          value={formValues.age}
+          name="email"
+          placeholder="Wusya-email"
+          value={formValues.email}
+          onChange={change}
         >
         </input>
         <input 
           type="text"
-          name="favFood"
-          placeholder="Favorite munchie"
-          value={formValues.favFood}
+          name="role"
+          placeholder="Wusyarole"
+          value={formValues.role}
+          onChange={change}
         >
         </input>
+        <input type="submit" value="submit" />
       </form>
     </div>
   );
